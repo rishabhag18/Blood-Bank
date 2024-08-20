c9c6111d8eb638aa353bd5a29dbb65336c3894c7
Akash Sen <iconicrick@gmail.com>
2024-08-20 09:09:47 +0530

    completed otp page code and firebase configuration

--- user/src/components/Forms/RegistrationForm/Steps/otpStep.jsx
+++ user/src/components/Forms/RegistrationForm/Steps/otpStep.jsx
@@ -1,11 +1,14 @@
 import { RecaptchaVerifier, signInWithPhoneNumber } from "firebase/auth";
-import { useState } from "react"
+import { useState } from "react";
 import { auth } from "./firebaseConfig";
 import { toast,Toaster } from "react-hot-toast";
 import {BsFillShieldLockFill,BsTelephoneFill} from "react-icons/bs";
-import {} from 'react-icons/cg';
+import {CgSpinner} from 'react-icons/cg';
+import OtpInput from "react-otp-input";
+import PhoneInput from "react-phone-input-2";
+import "react-phone-input-2/lib/style.css"
 
-const verifyUser = () => {
+const VerifyUser = () => {
     const[otp,setOtp] = useState("");
     const[ph,setPh] = useState("");
     const[loading,setLoading] = useState(false);
@@ -18,8 +21,9 @@ const verifyUser = () => {
                 "recaptcha-container",
                 {
                     size : "invisible",
-                    callback : (response) => {
+                    callback : (res) => {
                         onSignup();
+                        console.log(res);
                     },
                     "expired-callback" : () => {},
                 },
@@ -48,6 +52,17 @@ const verifyUser = () => {
             });
     }
 
+    function onOTPVerify() {
+        setLoading(true);
+        window.confirmationResult
+        .confirm(otp)
+        .then(async (res) => {
+            console.log(res);
+            setUser(res.user);
+            setLoading(false);
+        });
+    }
+
     return (
         <section className="flex items-center justify-center h-screen">
             <div>
@@ -67,7 +82,39 @@ const verifyUser = () => {
                             <label htmlFor="otp" className = "font-bold text-xl text-white text-center">
                                 Enter your OTP
                             </label>
+                            <OtpInput
+                            value={otp}
+                            onChange={setOtp}
+                            OTPLength={6}
+                            otpType="number"
+                            disabled={false}
+                            autofocus
+                            className="otp-container"></OtpInput>
+                            <button onClick={onOTPVerify} className="bg-red-600 w-full flex-gap-1 items-center justify-center py-2.5 text-white rounded">
+                                {loading && (
+                                    <CgSpinner size={20} className="mt-1 animate-spin"></CgSpinner>
+                                )}
+                                <span>Verify OTP</span>
+                            </button>
                             </>
+                        ) : (
+                            <>
+                                <div className="bg-white text-red-500 w-fit mx-auto p-4 rounded-full">
+                                    <BsTelephoneFill size={30}></BsTelephoneFill>
+                                </div>
+                                <label htmlFor="" className="font-bold text-xl text-white text-center">
+                                   Verify Your Phone Number 
+                                </label>
+                                <PhoneInput country={"in"} value={ph} onChange={setPh}></PhoneInput>
+                                <button onClick={onSignup}
+                                className="bg-red-600 w-full felx gap-1 items-center justify-center py-2.5 text-white rounded">
+                                    {loading && (
+                                    <CgSpinner size={20} className="mt-1 animate-spin"></CgSpinner>
+                                )}
+                                <span>Send code via SMS</span>
+                                </button>
+                            </>
+                        
                         )}
                     </div>
                     )}
@@ -76,4 +123,4 @@ const verifyUser = () => {
     );
 };
 
-export default verifyUser;
+export default VerifyUser;
